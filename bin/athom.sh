64694a6b615efddf9b304268a64f754acb327bd9
Omen Wild <omen@mandarb.com>
2025-04-21 21:11:52 -0700

    Local changes to have a templated build/install system for new PS01 devices.

---
+++ bin/athom.sh
@@ -0,0 +1,38 @@
+#! /bin/zsh -eu
+
+setopt warn_create_global
+
+PROGRAM=${0:t}
+
+if [[ $# -lt 1 ]]; then
+    echo "$PROGRAM: usage"
+    echo "$PROGRAM Room Name"
+    exit 1
+fi
+
+room="$@"
+friendly_name="$room Presence Sensor"
+name=$(echo $friendly_name | tr 'A-Z' 'a-z' | tr ' ' '-')
+
+source venv-esphome/bin/activate
+
+cp -a athom-configs/athom-presence-sensor.yaml .
+
+perl -pi -e "s/\@\@\@NAME\@\@\@/$name/; s/\@\@\@FRIENDLYNAME\@\@\@/$friendly_name/; s/\@\@\@ROOM\@\@\@/$room/" athom-presence-sensor.yaml
+
+less athom-presence-sensor.yaml
+
+esphome compile athom-presence-sensor.yaml
+
+echo
+cp -a .esphome/build/$name/.pioenvs/$name/firmware.bin $name.bin
+ls -l $name.bin
+
+device="${name}.iot.mandarb.com"
+
+cat <<EOF
+To flash this device, run the following command:
+$(host $device)
+
+esphome run --device $device athom-presence-sensor.yaml && rm $name.bin athom-presence-sensor.yaml
+EOF
